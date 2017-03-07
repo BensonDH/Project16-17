26ba7b8e472095498c541f4107d39fee8f51f16a
BensonDH <benson.deheel@student.kuleuven.be>
2017-03-07 21:46:09 +0100

    Add files via upload

---
+++ OGP1617-Asteroids/tests/asteroids/tests/ShipTests.java
@@ -0,0 +1,18 @@
+package asteroids.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShipTests {
+	
+	@Before
+	Ship Ship1 = new Ship();
+	Ship Ship2 = new Ship();
+
+	@Test
+	public void DistanceBetweenTests() {
+		
+	}
+
+}
